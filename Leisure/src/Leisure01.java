7dc08c6696a2153c9d1db235ed04878ee5d580bb
Leisure123 <105021011@live.asia.edu.tw>
2016-09-12 15:51:12 +0800

    Leisure homework submit
    the first homework

---
+++ Leisure/src/Leisure01.java
@@ -0,0 +1,4 @@
+
+public interface Leisure01 {
+
+}
---
+++ Leisure00/src/Leisure01.java
@@ -0,0 +1,12 @@
+
+public class Leisure01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("我 是 亞 大 資 訊 工 程 學 系 數 位 內 容 組 的 丁 紹 剛 ");
+	}
+
+}
